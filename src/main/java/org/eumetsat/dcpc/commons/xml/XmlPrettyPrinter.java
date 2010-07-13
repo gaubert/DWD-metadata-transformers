cda214f7cf95c51be68608dd9fd21e99fcdad6c1
Guillaume Aubert <guillaume.aubert@eumetsat.int>
2010-07-13 14:13:03 +0200

    create pretty printer

---
+++ src/main/java/org/eumetsat/dcpc/commons/xml/XmlPrettyPrinter.java
@@ -0,0 +1,110 @@
+package org.eumetsat.dcpc.commons.xml;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+
+/**
+ * XMLPrettyPrinter
+ * @author guillaume.aubert@eumetsat.int
+ *
+ */
+public class XmlPrettyPrinter
+{
+    /**
+     * Pretty Print the XML.
+     * @param aFilePath
+     * @return the XML document as a String
+     * @throws Exception if the source file doesn't exist 
+     *                   or if there is problem while doing the XML transformation
+     */
+    public static String prettyPrintAsString(String aFilePath) throws Exception
+    {
+        FileInputStream in = new FileInputStream(aFilePath);
+        
+        Document doc = XmlPrettyPrinter.loadXMLFrom(in);
+            
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+            
+        XmlPrettyPrinter.serialize(doc, out);
+            
+        return out.toString();
+    }
+    
+    /**
+     * Pretty Print the XML as an OutputStream
+     * @param aFilePath
+     * @param aOut
+     * @return the passed OutputStream filled
+     * @throws Exception if the source file doesn't exist 
+     *                   or if there is problem while doing the XML transformation
+     */
+    public static OutputStream prettyPrintString (String aFilePath, OutputStream aOut) throws Exception
+    {
+        FileInputStream in = new FileInputStream(aFilePath);
+        
+        Document doc = XmlPrettyPrinter.loadXMLFrom(in);
+            
+        XmlPrettyPrinter.serialize(doc, aOut);
+            
+        return aOut;
+    }
+    
+    private static void serialize(Document doc, OutputStream out) throws Exception
+    {
+
+        TransformerFactory tfactory = TransformerFactory.newInstance();
+        Transformer serializer;
+        try
+        {
+            serializer = tfactory.newTransformer();
+            // Setup indenting to "pretty print"
+            serializer.setOutputProperty(OutputKeys.INDENT, "yes");
+            serializer.setOutputProperty(
+                    "{http://xml.apache.org/xslt}indent-amount", "2");
+
+            serializer.transform(new DOMSource(doc), new StreamResult(out));
+        }
+        catch (TransformerException e)
+        {
+            // this is fatal, just dump the stack and throw a runtime exception
+            e.printStackTrace();
+
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static org.w3c.dom.Document loadXMLFrom(String xml)
+            throws org.xml.sax.SAXException, java.io.IOException
+    {
+        return loadXMLFrom(new java.io.ByteArrayInputStream(xml.getBytes()));
+    }
+
+    private static org.w3c.dom.Document loadXMLFrom(java.io.InputStream is)
+            throws org.xml.sax.SAXException, java.io.IOException
+    {
+        javax.xml.parsers.DocumentBuilderFactory factory = javax.xml.parsers.DocumentBuilderFactory
+                .newInstance();
+        factory.setNamespaceAware(true);
+        javax.xml.parsers.DocumentBuilder builder = null;
+        try
+        {
+            builder = factory.newDocumentBuilder();
+        }
+        catch (javax.xml.parsers.ParserConfigurationException ex)
+        {
+        }
+        org.w3c.dom.Document doc = builder.parse(is);
+        is.close();
+        return doc;
+    }
+}
--- src/test/java/org/eumetsat/dcpc/md/test/ExportTest.java
+++ src/test/java/org/eumetsat/dcpc/md/test/ExportTest.java
@@ -1,8 +1,15 @@
 package org.eumetsat.dcpc.md.test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.eumetsat.dcpc.commons.xml.XmlPrettyPrinter;
 import org.eumetsat.dcpc.md.export.ApplyXslt;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import junit.framework.TestCase;
 
@@ -15,7 +22,7 @@ public class ExportTest extends TestCase
         System.out.println("Hello world");
     }
 
-    public void testUniqueFile()
+    public void ztestUniqueFile()
     {
         String dirPath = TEST_DIR + File.separatorChar + "uniqueXML";
         String filePath = "H:/Dev/ecli-workspace/DWD-metadata-transformers/ext/xslt/eum2isoapFull_v3.xsl";
@@ -54,6 +61,24 @@ public class ExportTest extends TestCase
            e.printStackTrace();
            fail("Received Exception " + e);
         }
-
+    }
+    
+    public void testPrettyPrintFile()
+    {
+        String dirPath  = TEST_DIR + File.separatorChar + "uniqueXML";
+        String filePath = dirPath + File.separatorChar + "10.xml";
+        
+        try
+        {
+            String result = XmlPrettyPrinter.prettyPrintAsString(filePath);
+            
+            System.out.println(result);
+            
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            fail("See Exception Stack Trace");
+        }
     }
 }
